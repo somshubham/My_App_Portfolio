ec7e24f631286eaf2241fe598b8760c7fb03ca8d
somshubham <somshubhams@gmail.com>
2016-07-08 20:07:51 +0530

    done the first portfolio app

--- app/src/main/java/com/example/som/myappportfolio/MainActivity.java
+++ app/src/main/java/com/example/som/myappportfolio/MainActivity.java
@@ -21,17 +21,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setSupportActionBar(toolbar);
 
 
-        Button b1 = (Button) findViewById(R.id.button);
+        Button b1 = (Button) findViewById(R.id.popular_movies_button);
         b1.setOnClickListener(this);
-        Button b2 = (Button) findViewById(R.id.button2);
+        Button b2 = (Button) findViewById(R.id.stock_hawk_button);
         b2.setOnClickListener(this);
-        Button b3 = (Button) findViewById(R.id.button3);
+        Button b3 = (Button) findViewById(R.id.build_it_bigger_button);
         b3.setOnClickListener(this);
-        Button b4 = (Button) findViewById(R.id.button4);
+        Button b4 = (Button) findViewById(R.id.make_your_app_material_button);
         b4.setOnClickListener(this);
-        Button b5 = (Button) findViewById(R.id.button5);
+        Button b5 = (Button) findViewById(R.id.go_ubiquitous_button);
         b5.setOnClickListener(this);
-        Button b6 = (Button) findViewById(R.id.button6);
+        Button b6 = (Button) findViewById(R.id.capstone_button);
         b6.setOnClickListener(this);
 
 
@@ -45,22 +45,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
         switch(v.getId()) {
-            case R.id.button:
+            case R.id.popular_movies_button:
                 Toast.makeText(MainActivity.this, "This button will launch popular movies app! ", Toast.LENGTH_SHORT).show();
                 break;
-            case R.id.button2:
+            case R.id.stock_hawk_button:
                 Toast.makeText(MainActivity.this, "This button will launch stock hawk app! ", Toast.LENGTH_SHORT).show();
                 break;
-            case R.id.button3:
+            case R.id.build_it_bigger_button:
                 Toast.makeText(MainActivity.this, "This button will launch build it bigger app! ", Toast.LENGTH_SHORT).show();
                 break;
-            case R.id.button4:
+            case R.id.make_your_app_material_button:
                 Toast.makeText(MainActivity.this, "This button will launch make your app material app! ", Toast.LENGTH_SHORT).show();
                 break;
-            case R.id.button5:
+            case R.id.go_ubiquitous_button:
                 Toast.makeText(MainActivity.this, "This button will launch go ubiquitous app! ", Toast.LENGTH_SHORT).show();
                 break;
-            case R.id.button6:
+            case R.id.capstone_button:
                 Toast.makeText(MainActivity.this, "This button will launch capstone app! ", Toast.LENGTH_SHORT).show();
                 break;
         }
